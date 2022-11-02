d6140bf431047b079c9e49c3f1ed24389636f3d8
Oleg Semyonov <oleg@prezly.com>
2022-11-02 17:53:52 +0400

    Return default export from package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export { Image, ImageProps } from './components/Image';
+import { Image } from './components/Image';
+
+// Retaining default export for back compatibility
+// eslint-disable-next-line import/no-default-export
+export default Image;
+export { ImageProps } from './components/Image';
 export * from './components/Uploadcare';
 export { UploadcareImageProps, UploadcareEffect, UploadcareImageDetails } from './types';
